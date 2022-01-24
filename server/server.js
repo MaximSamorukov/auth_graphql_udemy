5104e59d53281154c0e3c959c038de32ab8a95d1
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-24 22:37:13 +0300

    part 12 wip

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://ralph_ralph:r1a2l3ph@cluster0.mlh5z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
